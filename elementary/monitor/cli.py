4f181fa792719d178ea97ad61c46f04a9dec8190
Thijs Nijhuis <thijs.nijhuis@shell.com>
2024-07-02 10:21:11 +0200

    debugging

--- elementary/monitor/cli.py
+++ elementary/monitor/cli.py
@@ -437,6 +437,7 @@ def report(
         target_path,
         dbt_quoting=dbt_quoting,
         env=env,
+        run_dbt_deps_if_needed=False
     )
     logger.info(f"In pass_context 2, config_dir '{config_dir}', profiles_dir '{profiles_dir}', project_dir '{project_dir}', update_dbt_package '{update_dbt_package}'")
     anonymous_tracking = AnonymousCommandLineTracking(config)
